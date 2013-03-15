4d9fe1a0dc240987bd2ca82c135f2dbd0858c29b
Ping <seymores@github.com>
2013-03-15 09:58:45 +0800

    Project version bump

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [twitter-api "0.7.1"]
-                 [com.taoensso/carmine "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [twitter-api "0.7.3"]
+                 [com.taoensso/carmine "1.6.0"]])
 
 ; https://github.com/ptaoussanis/carmine
